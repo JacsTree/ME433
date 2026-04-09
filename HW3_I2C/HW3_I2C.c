83de822e15c91a0f7966cd18b012291853925d14
JacsTree <jacobwood2028@u.northwestern.edu>
2026-04-09 06:47:43 -0500

    Finished HW3

--- HW3_I2C/HW3_I2C.c
+++ HW3_I2C/HW3_I2C.c
@@ -2,9 +2,6 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
@@ -20,8 +17,18 @@ void setReg(uint8_t reg ,uint8_t value){
     i2c_write_blocking(I2C_PORT, ADDR, buf, 2, false);
 }
 
-unsigned char readPin(unsigned char address, unsigned char register){
+uint8_t readReg(uint8_t reg){
+    uint8_t buf;
+    i2c_write_blocking(I2C_PORT, ADDR, &reg, 1, true);  // true to keep host control of bus
+    i2c_read_blocking(I2C_PORT, ADDR, &buf, 1, false);  // false - finished with bus
+    return buf;
+}
 
+uint8_t readPin(uint8_t reg, uint8_t pin){
+    uint8_t buf=readReg(reg);
+    // use bitwise AND to check if the specific pin is active
+    uint8_t mask = 1 << pin;
+    return (buf & mask) != 0;//returns 1 if active and 0 if not
 }
 
 int main()
@@ -33,9 +40,9 @@ int main()
     
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    // we are using external pullup resistors
     //gpio_pull_up(I2C_SDA);
     //gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
     //write:
     //i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
@@ -47,11 +54,12 @@ int main()
     // setup
     setReg(0x00, 0x7F);// set IO direction to read except for GP7 write
 
-    setReg(0x09,0x80);// set GP7 to high
-
     while (true) {
-        //printf("Hello, world!\n");
+        uint8_t buffer = readPin(0x09,0);
+        //printf("char= %d\n",buffer);
         
-        sleep_ms(1000);
+        if (buffer==0) setReg(0x09,0x80);
+        else setReg(0x09,0x00);
+        //sleep_ms(1);
     }
 }
