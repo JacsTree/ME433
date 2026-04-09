226a2ba5276eb93cd3ddda7feafa1dddc0a55a91
JacsTree <jacobwood2028@u.northwestern.edu>
2026-04-09 05:50:31 -0500

    Used I2C to make blue led on gp7 go high

--- HW3_I2C/HW3_I2C.c
+++ HW3_I2C/HW3_I2C.c
@@ -5,11 +5,24 @@
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define LED_PIN 16
+
+#define ADDR 0x20
 
 
+void setReg(uint8_t reg ,uint8_t value){
+    uint8_t buf[2];
+    buf[0] = reg;
+    buf[1] = value;
+    i2c_write_blocking(I2C_PORT, ADDR, buf, 2, false);
+}
+
+unsigned char readPin(unsigned char address, unsigned char register){
+
+}
 
 int main()
 {
@@ -20,12 +33,25 @@ int main()
     
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
+    //gpio_pull_up(I2C_SDA);
+    //gpio_pull_up(I2C_SCL);
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
+    //write:
+    //i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
+
+    // read:
+    // i2c_write_blocking(i2c_default, ADDR, &reg, 1, true);  // true to keep host control of bus
+    // i2c_read_blocking(i2c_default, ADDR, &buf, 1, false);  // false - finished with bus
+
+    // setup
+    setReg(0x00, 0x7F);// set IO direction to read except for GP7 write
+
+    setReg(0x09,0x80);// set GP7 to high
+
     while (true) {
-        printf("Hello, world!\n");
+        //printf("Hello, world!\n");
+        
         sleep_ms(1000);
     }
 }
