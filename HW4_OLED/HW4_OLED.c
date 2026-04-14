0dc9344bd4ab5e478b1dc0c9fb35798c973e50b5
JacsTree <jacobwood2028@u.northwestern.edu>
2026-04-14 15:49:13 -0500

    fps and voltage

--- HW4_OLED/HW4_OLED.c
+++ HW4_OLED/HW4_OLED.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include "hardware/adc.h"
 #include "ssd1306.h"
 #include "font.h"
 
@@ -33,27 +34,39 @@ int main()
 {
     stdio_init_all();
 
-    i2c_init(I2C_PORT, 400*1000);
+    i2c_init(I2C_PORT, 2100*1000);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    adc_init();
+    adc_gpio_init(26);
+    adc_select_input(0);
 
     // setup
     sleep_ms(10);
     ssd1306_setup();
 
-    int i = 15;
-    char message[50]; 
-    sprintf(message, "my var = %d", i); 
- // draw starting at x=20,y=10  
+    unsigned int time1 = to_us_since_boot(get_absolute_time());
+    float frames = 0;
     while (true) {
-        draw_message(20,10,message);
-        ssd1306_update();
-        sleep_ms(500);
-        
-        ssd1306_clear();
-        ssd1306_update();
-        sleep_ms(500);
-        
+        float result = (adc_read() / (float)4095) * 3.3;
+        char message[50]; 
+        sprintf(message, "Voltage = %.2f", result); 
+        draw_message(0, 0, message);
 
+        unsigned int time2 = to_us_since_boot(get_absolute_time());
+        frames++;
+
+        if (time2 - time1 > 500000) {
+            float elapsed_time = (time2 - time1) / 1000000.0;
+            float fps = frames / elapsed_time; 
+            time1 = time2;
+            frames = 0;
+            
+            char message2[50]; 
+            sprintf(message2, "FPS = %.2f", fps); 
+            draw_message(0, 25, message2);
+        }
+
+        ssd1306_update();
     }
 }
