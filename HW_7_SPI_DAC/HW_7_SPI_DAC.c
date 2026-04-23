08e104b74fc20a8e3c5877240d34e07eec9a0908
JacsTree <jacobwood2028@u.northwestern.edu>
2026-04-23 16:54:46 -0500

    waves working

--- HW_7_SPI_DAC/HW_7_SPI_DAC.c
+++ HW_7_SPI_DAC/HW_7_SPI_DAC.c
@@ -24,12 +24,11 @@ static inline void cs_deselect() {
  */
 static inline void put_dac(uint8_t adc_select, uint16_t voltage){
     uint8_t buf[2];
-    buf[0] = ((0b10000000&adc_select)<<15)|0b00110000|(0b00001111 & (voltage >> 6));
+    buf[0] = (0b10000000&((adc_select)<<7))|0b00110000|(0b00001111 & (voltage >> 6));
     buf[1] = (voltage<<2)&0b11111100;
     cs_select();
     spi_write_blocking(spi_default, buf, 2);
     cs_deselect();
-    //sleep_ms(10);//?
 }
 
 int main()
@@ -49,15 +48,16 @@ int main()
         voltages_0[i]=(uint16_t)(1023.9999*(i/199.0));
         voltages_0[399-i]=(uint16_t)(1023.9999*(i/199.0));
     }
-    // for(int i = 0; i<400;i++){
-    //     voltages_1[i]=(uint16_t)(1023.9999*((sinf((i*2*3.14159)/399)/2)+1));
-    // }
+    for (int i = 0; i<400; i++) {
+        voltages_1[i] = (uint16_t)(((sinf((2.0f * M_PI * i) / 400.0f) * 0.5f) + 0.5f) * 1023.0f + 0.5f);
+    }
 
+    int idx0 = 0, idx1 = 0;
     while (true) {
-        for(int i = 0; i<400; i+=4){
-            put_dac(0,voltages_0[i]);
-            //put_dac(1,voltages_1[i]);
-            sleep_us(5000);
-        }   
+        put_dac(0, voltages_0[idx0]);
+        put_dac(1, voltages_1[idx1]);
+        sleep_us(2500);
+        idx0 = (idx0 + 1) % 400;
+        idx1 = (idx1 + 2) % 400;
     }
 }
