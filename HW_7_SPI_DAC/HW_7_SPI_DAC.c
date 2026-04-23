6936beddf784a99442986b114244efd4d84be249
JacsTree <jacobwood2028@u.northwestern.edu>
2026-04-23 14:36:42 -0500

    voltage out works, trying both wave types

--- HW_7_SPI_DAC/HW_7_SPI_DAC.c
+++ HW_7_SPI_DAC/HW_7_SPI_DAC.c
@@ -1,13 +1,63 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "math.h"
 
+#define PIN_CS 17
+
+static inline void cs_select() {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(PIN_CS, 0);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
+
+static inline void cs_deselect() {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(PIN_CS, 1);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
+
+/**
+ * Writes to the DAC
+ * @param adc_select (0 or 1)
+ * @param voltage (0-1023)
+ */
+static inline void put_dac(uint8_t adc_select, uint16_t voltage){
+    uint8_t buf[2];
+    buf[0] = ((0b10000000&adc_select)<<15)|0b00110000|(0b00001111 & (voltage >> 6));
+    buf[1] = (voltage<<2)&0b11111100;
+    cs_select();
+    spi_write_blocking(spi_default, buf, 2);
+    cs_deselect();
+    //sleep_ms(10);//?
+}
 
 int main()
 {
     stdio_init_all();
+    spi_init(spi_default, 1000 * 1000 * 10); // the baud, or bits per second
+    gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
+    gpio_init(PIN_CS);
+    gpio_set_dir(PIN_CS,1);
+    gpio_put(PIN_CS,1);
+
+    uint16_t voltages_0[400];
+    uint16_t voltages_1[400];
+    for(int i = 0; i<200;i++){
+        voltages_0[i]=(uint16_t)(1023.9999*(i/199));
+        voltages_0[399-i]=(uint16_t)(1023.9999*(i/199));
+    }
+    for(int i = 0; i<400;i++){
+        voltages_1[i]=(uint16_t)(1023.9999*((sinf((i*2*3.14159)/399)/2)+1));
+    }
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        for(int i = 0; i<400; i+=4){
+            put_dac(0,voltages_0[i]);
+            put_dac(1,voltages_1[i]);
+            sleep_us(1000);
+        }   
     }
 }
